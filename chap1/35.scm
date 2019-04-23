2c39132728c8b5c1abbad2d5ee9777f163db0c3d
Michael T <bravmech@gmail.com>
2019-04-23 17:22:39 +0500

    initial commit

---
+++ chap1/35.scm
@@ -0,0 +1,12 @@
+
+(load "../utils.scm")
+(load "fixed-point.scm")
+
+
+; phi
+(check-within (fixed-point (lambda (x)
+                                   (+ 1 (/ 1 x)))
+                           1.0)
+              1.618
+              0.001)
+(test)
