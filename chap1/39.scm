7d0b3a28e309da80e150f10ea97faec2fb60b432
Michael T <bravmech@gmail.com>
2019-04-24 18:35:55 +0500

    initial commit

---
+++ chap1/39.scm
@@ -0,0 +1,19 @@
+
+(load "../utils.scm")
+(load "cont-frac.scm")
+
+
+(define (tan-cf x k)
+  (define (n i)
+    (if (= i 1)
+        x
+        (- (* x x))))
+  (define (d i)
+    (- (* 2 i) 1))
+  (cont-frac n d k))
+
+
+(check-within (tan-cf 2.0 20)
+              -2.185
+              0.001)
+(test)
---
+++ chap1/cont-frac.scm
@@ -0,0 +1,19 @@
+
+(define (cont-frac n d k)
+  (define (rec i)
+    (if (> i k)
+        0
+        (/ (n i)
+           (+ (d i)
+              (rec (+ i 1))))))
+  (rec 1))
+
+; iter bottom-up
+(define (cont-frac-iter n d k)
+  (define (iter i result)
+    (if (= i 0)
+        result
+        (iter (- i 1)
+              (/ (n i)
+                 (+ (d i) result)))))
+  (iter k 0))
