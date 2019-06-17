a890749e0dbfdec1dbbd3d8bd206416fe8a3b85e
Michael T <bravmech@gmail.com>
2019-06-17 18:34:19 +0500

    count-change-list

--- chap1/count-change.scm
+++ chap1/count-change.scm
@@ -19,5 +19,18 @@
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
 
+(define (count-change-list amount)
+  (cc-list amount 5))
+(define (cc-list amount kinds-of-coins)
+  (cond ((= amount 0) '(()))
+        ((or (< amount 0) (= kinds-of-coins 0)) '())
+        (else (append (cc-list amount (- kinds-of-coins 1))
+                      (let ((denom (first-denomination kinds-of-coins)))
+                        (map (lambda (a) (cons denom a))
+                             (cc-list (- amount denom)
+                                 kinds-of-coins)))))))
 
-(assert= (count-change 100) 292)
+
+(check-expect (count-change 100) 292)
+(check-expect (length (count-change-list 100)) 292)
+(test)
