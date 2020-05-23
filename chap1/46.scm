2f55c587c5c17fd0069e592eb24708d25ecde953
Michael T <bravmech@gmail.com>
2020-05-23 11:23:54 +0500

    format

--- chap1/46.scm
+++ chap1/46.scm
@@ -1,7 +1,5 @@
-
 (load "../utils.scm")
 (load "iterative-improve.scm")
-
 
 (define (fixed-point f first-guess)
   ((iterative-improve close-enough? f) first-guess))
@@ -9,10 +7,9 @@
 (define (average-damp f)
   (lambda (x) (average x (f x))))
 (define (sqrt x)
-  ((iterative-improve close-enough?           
+  ((iterative-improve close-enough?
                       (average-damp (lambda (y) (/ x y))))
    1.0))
-
 
 ; x**x = 1000
 (check-within (fixed-point (lambda (x)
